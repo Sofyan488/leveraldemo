7b59baaaef520eb854c3b56aa965ff49c8aceb9a
Sofyan488 <s12217431@stu.najah.edu>
2025-05-18 19:12:20 +0300

    tes

--- sofyan/app/Models/System.php
+++ sofyan/app/Models/System.php
@@ -1,14 +1,1 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-class System extends Model
-{
-    use HasFactory;
-    protected $table = 'system_settings'; 
-    protected $primaryKey = 'settings_id';
-    public $timestamps = false;
-
-}
+ؤ
